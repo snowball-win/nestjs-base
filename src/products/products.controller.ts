f63c199ea5603738ae28d1a1788eed66522505c0
李俊杰 <1091457391@qq.com>
2023-04-06 23:45:14 +0800

    feat: get post 请求 带参数

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, Query, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
     // this.productsService = new ProductsService() 等价于 constructor 方式
     constructor(private productsService: ProductsService){}
-    @Get()
+    @Get('/getProductsList')
     getProductsList(): any{
         return this.productsService.getProductsList()
+    }
+    @Post('/addProducts')
+    addProducts(): any{
+        return this.productsService.addProducts()
+    }
+    // @Post('/addProducts2')
+    // addProducts2(@Request() req): any{
+    //     return this.productsService.addProducts2()
+    // }
+    // post带参数
+    @Post('/addProducts2')
+    addProducts2(@Body() body): any{
+        let id = body.id
+        return this.productsService.addProducts2(id)
+    }
+    @Get('/getProductsById')
+    // @Request()装饰器
+    // getProductsById(@Request() req):any{
+    //     let id:number = parseInt(req.query.id)
+    //     return this.productsService.getProductsById(id)
+    // }
+    // @Query()装饰器
+    getProductsById(@Query() query):any{
+        let id:number = parseInt(query.id)
+        return this.productsService.getProductsById(id)
     }
 }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,4 +9,46 @@ export class ProductsService {
             msg: '请求产品列表成功'
         }
     }
+    addProducts(){
+        return {
+            code: 200,
+            data: ['huawei', 'changcheng', 'biyadi'],
+            msg: '增加产品成功'
+        }
+    }
+    addProducts2(id: any){
+        console.log(id)
+        let resJson:any = {
+            code: 200,
+            data: ['huawei', 'changcheng', 'biyadi'],
+            msg: '增加产品成功'
+        }
+        switch (id) {
+            case 1:
+                resJson.data = ['huawei']
+            break;
+            case 2:
+                resJson.data = ['changcheng']
+            break;
+            case 3:
+                resJson.data = ['biyadi']
+            break;
+        }
+        return resJson
+    }
+    getProductsById(id:number){
+        let resJson:any = {}
+        switch (id) {
+            case 1:
+                resJson = {id:1, name: 'huawei', type: 'shouji'}
+            break;
+            case 2:
+                resJson = {id:2, name: 'changcheng', type: 'qiche'}
+            break;
+            case 3:
+                resJson = {id:3, name: 'biyadi', type: 'qiche'}
+            break;
+        }
+        return resJson;
+    }
 }
